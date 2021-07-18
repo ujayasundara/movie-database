7832007a2cf50611422b39aadbe2b1658ac6bb1d
Udayanga Jayasundara <udayangajayasundara@gmail.com>
2021-07-18 09:06:21 -0400

    globle styles added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
-const Star = () => React.createElement('div',null, 'This is first');
+//Styles
+import {GlobalStyle} from './GlobalStyle';
 
-const App = () => {
-  return Star();
+function App() {
+  return (
+    <div className="App">
+      Start here.
+      <GlobalStyle />
+    </div>
+  );
 }
 
 export default App;
